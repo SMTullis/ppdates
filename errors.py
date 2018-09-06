8a1cb7b446bc958d51c998fb38a5658d3108e9fb
Sean M Tullis <stullis@fs.fed.us>
2018-09-06 07:42:38 -0600

    Add PayPeriodError and YearUnknownError

--- errors.py
+++ errors.py
@@ -1,5 +1,21 @@
 class PayPeriodError(Exception):
+    """PayPeriodError is returned any time that the pay period number exceeds
+    the number of pay periods in a given year.
+    """
 
-    def __init__(self):
-        pass
-        
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
+
+class YearUnknownError(Exception):
+    """YearUnknownError is raised whenever data about a year is requested and
+    it is not found in the year list.
+    """
+
+    def __init__(self, error):
+        self.error = error
+
+    def __str__(self):
+        return repr(self.error)
